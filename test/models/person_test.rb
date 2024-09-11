c16ebf15e4d3fb6d08b73299b968bd79fe21bc18
Joshua Born <joshua.born@gmail.com>
2024-09-11 15:40:25 -0400

    Create Person model

---
+++ test/models/person_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class PersonTest < ActiveSupport::TestCase
+   test "should not save without a name" do
+    assert_not Person.new.save, "saved the person without a name"
+   end
+end
