5594cd2f45cff75a8a2d04e5ca3a826ae386b89c
Joshua Born <joshua.born@gmail.com>
2024-09-19 15:34:05 -0400

    Create Expense.split_between_two_people

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -13,8 +13,26 @@ class Expense < ApplicationRecord
   def dollar_amount_paid=(dollars)
     self.amount_paid = (100 * dollars).to_i
   end
-  # class << self
-  # end
+
+  class << self
+    def split_between_two_people(payer, ower, dollar_amount)
+      expense = Expense.new(dollar_amount_paid: dollar_amount)
+      half_amount = (dollar_amount * 100) / 2
+      if half_amount % 1 == 0 then
+        expense.person_expenses.new(person: ower, amount: -half_amount)
+        expense.person_expenses.new(person: payer, amount: half_amount)
+      else
+        if rand() <= 0.5 then
+          expense.person_expenses.new(person: ower, amount: -half_amount.ceil)
+          expense.person_expenses.new(person: payer, amount: half_amount.floor)
+        else
+          expense.person_expenses.new(person: ower, amount: -half_amount.floor)
+          expense.person_expenses.new(person: payer, amount: half_amount.ceil)
+        end
+      end
+      expense
+    end
+  end
 
   # private
 end
