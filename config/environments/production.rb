db200182bd9dc052620620f77b60e42808e150d1
Joshua Born <joshua.born@gmail.com>
2024-10-07 17:28:23 -0400

    Rename application to "Bentley Needs Money"

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter = :resque
-  # config.active_job.queue_name_prefix = "splitwise_replacement_production"
+  # config.active_job.queue_name_prefix = "bentley_needs_money_production"
 
   # Disable caching for Action Mailer templates even if Action Controller
   # caching is enabled.
